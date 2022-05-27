f1e61465d71b6cfc1a7b452d8743bd093f41dc1f
StephanHolding <stephanholding@gmail.com>
2022-05-27 15:39:10 +0200

    Added answer models of all riddles

---
+++ Riddle_Game 5.0/Source/Riddle_Game/Riddle_Game.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Riddle_Game.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Riddle_Game, "Riddle_Game" );
